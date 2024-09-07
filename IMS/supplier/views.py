0268d2d21799d4308825f7fec741e5dfd06acf69
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-07 16:21:26 +0530

    suppliers completion

--- IMS/supplier/views.py
+++ IMS/supplier/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 from supplier.models import Supplier
+from inventory.models import Purchase
 
 # Create your views here.
 def view_suppliers(request):
     suppliers = Supplier.objects.all()
-    return render(request,template_name='suppliers.html',context={'data':suppliers})
+    return render(request,template_name='suppliers.html',context={'suppliers':suppliers})
 
 def get_supplier(request,id):
     supplier = Supplier.objects.get(id=id)
-    return render(request,template_name='supplier.html',context={'data':supplier})
+    orders = Purchase.objects.filter(supplier=supplier)
+    if request.method == 'POST':
+        rating = supplier.rating
+        rating.commitment = request.POST['commitment']
+        rating.consistency = request.POST['consistency']
+        rating.transaction = request.POST['transaction']
+        rating.cost = request.POST['cost']
+        rating.competency = request.POST['competency']
+        rating.communication = request.POST['communication']
+        rating.save()
+        return HttpResponseRedirect('')
+    else:
+        return render(request,template_name='supplier.html',context={'supplier':supplier,'orders':orders})
