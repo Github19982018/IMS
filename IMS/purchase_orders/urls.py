003a2a3dd0902f34a8337ffccfcf56f3d581103b
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-07 22:23:15 +0530

    purchase orders updations

--- IMS/purchase_orders/urls.py
+++ IMS/purchase_orders/urls.py
@@ -4,6 +4,6 @@ from purchase_orders import views
 urlpatterns = [
     path('',views.view_purchases,name='purchases'),
     # path('<int:id>/',views.get_purchase,name='purchase'),
-    path('add/',views.add_purchase,name='add_purchase'),
+    path('add/<int:id>/',views.add_purchase,name='add_purchase'),
 ]
  
--- IMS/purchase_orders/views.py
+++ IMS/purchase_orders/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from purchase_orders.models import Purchase,Purchase_status
+from inventory.models import Inventory
+from supplier.models import Supplier
+
 
 # Create your views here.
 def view_purchases(request):
     purchases = Purchase.objects.all()
     return render(request,template_name='purchases.html',context={'purchases':purchases})
 
-def add_purchase(request):
-    return render(request,template_name='purchase.html')
+def add_purchase(request,id):
+    item = Inventory.objects.get(id=id)
+    supplier = Supplier.objects.filter()
+    if request.method == 'POST':
+        return render(request,template_name='purchase_next.html',context={'item':item})
+    else:
+        return render(request,template_name='purchase.html',context={'item':item,'supplier':supplier})
