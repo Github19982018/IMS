8130e88588d470165c6f59c8f1390df3965c8b36
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-13 22:59:06 +0530

    added warehouse selection

--- IMS/inventory/middleware/custom_middleware.py
+++ IMS/inventory/middleware/custom_middleware.py
@@ -1,11 +1,17 @@
 from inventory.models import Warehouse
 
 class Warehouse_middleware:
-    warehouse = 1
+    with open('data/warehouse.txt','+w') as wfile:
+        wfile.write('1')
+
     def __init__(self,get_response): 
         self.get_response = get_response
+        with open('data/warehouse.txt','r+') as wfile:
+            self.warehouse = wfile.read()
     def __call__(self, request):
         self.warehouse = request.GET.get('warehouse') or self.warehouse
+        with open('data/warehouse.txt','+w') as wfile:
+            wfile.write(self.warehouse)
         request.w = self.warehouse
         response = self.get_response(request)
         return response
@@ -13,5 +19,6 @@ class Warehouse_middleware:
     def process_template_response(self,request, response):
         warehouses = Warehouse.objects.all()
         response.context_data['warehouses'] = warehouses
-        response.context_data['w'] = self.warehouse
+        response.context_data['w'] = int(self.warehouse)
         return response
+
