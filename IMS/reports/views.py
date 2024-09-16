378bec4f11079661cf87ee3650e119fb472c426c
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-16 19:16:00 +0530

    report  updates

--- IMS/reports/views.py
+++ IMS/reports/views.py
@@ -15,15 +15,15 @@ def purchase(request):
     filterby = request.GET.get('by')
     purchases = ''
     if filterby == 'item':
-        purchases = PurchaseItems.objects.values('item_id').annotate(amount=Sum('price'),quantity=Sum('quantity'),price=Avg('price'))
+        purchases = PurchaseItems.objects.values('item','item__name').annotate(amount=Sum('price'),quantity=Sum('quantity'),price=Avg('price'))
     return render(request,'report_purchase.html',{'purchase':purchases})
 
 def sales(request):
     filterby = request.GET.get('by')
     if filterby == 'item':
-        sales = SalesItems.objects.values('item_id','item_id__name').annotate(amount=Sum('price'),price=Avg('price'),quantity=Sum('quantity'))
+        sales = SalesItems.objects.values('item','item__name').annotate(amount=Sum('price'),price=Avg('price'),quantity=Sum('quantity'))
     elif filterby == 'customer':
-        sales = SalesItems.objects.values('customer','item_id__name','customer__name').annotate(amount=Sum('price'),price=Avg('price'),quantity=Sum('quantity'))
+        sales = SalesItems.objects.values('sales__customer','item__name','sales__customer__name').annotate(amount=Sum('price'),price=Avg('price'),quantity=Sum('quantity'))
     return render(request,'report_sales_customer.html',{'sales':sales})
 
 
