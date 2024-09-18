7f216e2362b55df6d26d7dd21df051ad87c0ea4a
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-18 21:33:02 +0530

    added env file and requirements file

--- IMS/IMS/settings.py
+++ IMS/IMS/settings.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
+
 """
+import environ
 
 from pathlib import Path
 
@@ -19,8 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+env = environ.Env()
+environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(-1kf5&gggy(d+q54o)zmv2g+*kmj-blu4q*itv)w_ye@(4qox'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,11 +92,11 @@ WSGI_APPLICATION = 'IMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'IMS',
-        'USER':'root',
-        'PASSWORD':'admin',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'NAME': env("DATABASE_NAME"),
+        'USER':env("DATABASE_NAME"),
+        'PASSWORD':env("DATABASE_PASSWORD"),
+        'HOST':env("DATABASE_HOST"),
+        'PORT':env("DATABASE_PASSWORD"),
         'CONN_MAX_Age':None
     }
 }
