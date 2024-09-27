4de7414aff03fa3ea667c63b2c3c4bc3dbfe0899
jobin kuriakose <jobinkuriakose@protonmail.com>
2024-09-27 23:07:54 +0530

    dashboard improvements

--- IMS/dashboard/views.py
+++ IMS/dashboard/views.py
@@ -3,27 +3,49 @@ from sales_orders.models import Package,PackageStatus,Sales,SalesStatus,Shipment
 from inventory.models import Inventory
 from django.db.models.lookups import GreaterThan,LessThan
 from django.db.models import Sum
-from purchase_orders.models import PurchaseOrder,Purchase_status,PurchaseItems
+from purchase_orders.models import PurchaseOrder,Purchase_status,PurchaseItems,PurchaseDraft
 from django.db import connection
+cursor = connection.cursor()
+
+READY_TO_SHIP = 0
+SENT_TO_FLEET = 1
+SENT_TO_CARRIER =2
+CARRIER_PICKED = 3
+CUSTOMER_RECEIVED = 4
+SHIP_CANCELLED = 5
+
+SALE_DRAFT = 1
+SALE_PACKED = 2
+SALE_SHIPPED = 3
+SALE_DELIVERED = 4
+PAYED = 5
+SALE_CANCELLED = 6
+
+PACKAGE_DRAFT = 1
+PACKAGE_PACKED = 2
+PACKAGE_READY_SHIP = 3
+PACKAGE_SHIPPED = 4
 
 # Create your views here.
 def dashboard(request):
-    pack = Package.objects.filter(status=PackageStatus(id=1)).count()
-    ship = Shipment.objects.filter(status=ShipStatus(id=1)).count()
-    deliver = Shipment.objects.filter(status=ShipStatus(id=4)).count()
+    pack = Package.objects.filter(status=PackageStatus(id=PACKAGE_DRAFT)).count()
+    ship = Shipment.objects.filter(status__in=ShipStatus.objects.filter(id__in=[0,1,2])).count()
+    deliver = Shipment.objects.filter(status=ShipStatus(id=CARRIER_PICKED)).count()
     on_hand = Inventory.objects.aggregate(on_hand=Sum('on_hand'))
-    to_receive = PurchaseOrder.objects.filter(status__in=[4,5]).count()
+    to_receive = PurchaseItems.objects.aggregate(Sum('quantity'))
+    cursor.execute( """select sum(quantity) from purchase_orders_purchaseitems pi join purchase_orders_purchasedraft pd on pi.purchase_id=pd.id join purchase_orders_purchaseorder po on pd.id=po.id_id where po.status_id<7; """)
+    ((to_receive,),) = cursor.fetchall()
+    
     purchase_items = PurchaseItems.objects.all().values('item').distinct().count()
     purchase_quantity = PurchaseItems.objects.aggregate(purchase_quantity=Sum('quantity'))
     purchase_amount = PurchaseOrder.objects.aggregate(purchase_amount=Sum('total_amount'))
     # print(purchase_amount,purchase_quantity)
     total_stock = Inventory.objects.all().count()
-    draft = PurchaseOrder.objects.filter(status=Purchase_status(id=2)).count()
-    packed = PurchaseOrder.objects.filter(status=Purchase_status(id=3)).count()
-    shipped = PurchaseOrder.objects.filter(status=Purchase_status(id=4)).count()
+    draft = Sales.objects.filter(status=SalesStatus(id=SALE_DRAFT)).count()
+    packed = Sales.objects.filter(status__in=SalesStatus.objects.filter(id__in=[2,3,4,5])).count()
+    shipped = Sales.objects.filter(status__in=SalesStatus.objects.filter(id__in=[3,4,5])).count()
     # cursor.execute('''SELECT count(id) FROM purchase where id>0 and id<)
 
-    cursor = connection.cursor()
     cursor.execute('''SELECT count(id) FROM inventory_inventory where on_hand<(reorder_point) and (on_hand>0)''')
     # low_stock = Inventory.objects.raw('''SELECT count(id) FROM inventory_inventory where on_hand<(reorder_point) and (on_hand>0)''')
     # low_stock = Inventory.objects.aaggregate(F('on_hand'))
